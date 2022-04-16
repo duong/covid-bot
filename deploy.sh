55b307bee9fc2788df59a2313d2298f8623aaf8a
Duong Dinh <dd.dinh11@gmail.com>
2022-04-16 14:26:09 +1000

    Added secret env variable TARGET_SERVERS

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,5 @@ gcloud functions deploy 'discord-covid-update' \
   --timeout='30s' \
   --runtime='nodejs14' \
   --project='duong-dev' \
-  --region='australia-southeast1'
+  --region='australia-southeast1' \
+  --set-secrets='TARGET_SERVERS=projects/374547579835/secrets/TARGET_SERVERS/versions/3'
